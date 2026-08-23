ed0d604aa986f24f3b741ae3fa38116d6076f2a8
Noelle-1220 <m.ingabire3@alustudent.com>
2026-08-23 22:26:15 +0400

    Task well done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
 """
-1-top_ten
+Queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit.
 """
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot
-    posts for a given subreddit. If invalid, prints None.
-    """
+    """Print the titles of first 10 hot posts, or None if invalid."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "custom-user-agent/0.1"}
+    headers = {
+        "User-Agent": "linux:alche-api-advanced-qz84n:v1.0 (by /u/evika_dev)"
+    }
     params = {"limit": 10}
 
-    try:
-        response = requests.get(
-            url, headers=headers, params=params, allow_redirects=False
-        )
-        if response.status_code != 200:
-            print(None)
-            return
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
 
-        posts = response.json().get("data", {}).get("children", [])
-        if not posts:
-            print(None)
-            return
-
-        for post in posts:
-            print(post.get("data", {}).get("title"))
-
-    except Exception:
+    if response.status_code != 200:
         print(None)
+        return
+
+    data = response.json().get("data")
+    if data is None:
+        print(None)
+        return
+
+    posts = data.get("children", [])
+    for post in posts[:10]:
+        print(post.get("data", {}).get("title"))
