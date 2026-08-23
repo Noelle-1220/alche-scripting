7219bb484e093f8c45349e46e6d9d53a04889500
Noelle-1220 <m.ingabire3@alustudent.com>
2026-08-23 22:28:19 +0400

    Task well done

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
 """
-Returns the number of subscribers for a subreddit.
+Queries the Reddit API and returns the number of
+subscribers for a given subreddit.
 """
+
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """Return the number of subscribers for a subreddit."""
+    base_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
     headers = {
-        "User-Agent": "Mozilla/5.0"
+        "User-Agent": "alche-api-advanced/1.0"
     }
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
+    response = requests.get(
+        base_url,
+        headers=headers,
+        allow_redirects=False
+    )
     if response.status_code != 200:
         return 0
-
-    data = response.json()
-
-    return data.get("data", {}).get("subscribers", 0)
+    try:
+        data = response.json()
+        return data["data"]["subscribers"]
+    except(KeyError, ValueError, TypeError):
+        return 0
