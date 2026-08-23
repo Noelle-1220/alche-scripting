bc87b66a3ccf39743a07916658b2b9f2fdaa1054
Noelle-1220 <m.ingabire3@alustudent.com>
2026-08-23 21:34:03 +0400

    Task well done

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+1-top_ten
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Print the titles of the first 10 hot posts."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    headers = {
+        "User-Agent": "python:alche-scripting:v1.0 (by /u/ALU)"
+    }
+
+    response = requests.get(
+        url,
+        headers=headers,
+        allow_redirects=False
+    )
+
+    if response.status_code != 200:
+        print("None")
+        return
+
+    posts = response.json().get("data", {}).get("children", [])
+
+    for post in posts[:10]:
+        print(post.get("data", {}).get("title"))
